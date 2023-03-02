ec561bee9947f78bf4128356a17a0b713d98c411
Decstro <juan.puello99@hotmail.com>
2023-03-02 15:38:24 -0500

    Fix: Package file

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
-import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import { TaskForm } from './TaskForm';
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
 
   return (
     <div>
-      <h1>To Do List</h1>
+      <h1>To do App!</h1>
+
+      <TaskForm/>
 
       <ul>
-        {tasks.map(task => (
-          <Task key={task._id} task={task} />
-        ))}
+        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
       </ul>
     </div>
   );
